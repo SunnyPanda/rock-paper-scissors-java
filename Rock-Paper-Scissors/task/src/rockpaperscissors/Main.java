10f7fe8585af5c69bfc551b63e649e6d7f3402a1
SunnyPanda <ekaterina.kozlova@canter.fi>
2023-02-21 15:11:15 +0200

    Stage 2/5: Equalizing chances

--- Rock-Paper-Scissors/task/src/rockpaperscissors/Main.java
+++ Rock-Paper-Scissors/task/src/rockpaperscissors/Main.java
@@ -1,14 +1,27 @@
 package rockpaperscissors;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Map<String, String> bits = new HashMap<>();
+        bits.put("rock", "scissors");
+        bits.put("paper", "rock");
+        bits.put("scissors", "paper");
+        String[] options = new String[]{"rock", "paper", "scissors"};
+
         Scanner in = new Scanner(System.in);
-        switch (in.next()) {
-            case "rock" -> System.out.println("Sorry, but the computer chose paper");
-            case "paper" -> System.out.println("Sorry, but the computer chose scissors");
-            case "scissors" -> System.out.println("Sorry, but the computer chose rock");
-        }
+        String user = in.next();
+
+        Random random = new Random();
+        int index = random.nextInt(3);
+        String comp = options[index];
+
+        if (user.equals(comp)) System.out.printf("There is a draw (%s)", user);
+        else if (comp.equals(bits.get(user))) System.out.printf("Well done. The computer chose %s and failed", comp);
+        else System.out.printf("Sorry, but the computer chose %s", comp);
     }
 }
