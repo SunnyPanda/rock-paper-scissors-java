d9b4dc9656010b9aa135b7a251c5ea5ea1fcf498
SunnyPanda <ekaterina.kozlova@canter.fi>
2023-02-21 14:58:28 +0200

    Stage 1/5: Unfair computer

--- Rock-Paper-Scissors/task/src/rockpaperscissors/Main.java
+++ Rock-Paper-Scissors/task/src/rockpaperscissors/Main.java
@@ -1,7 +1,14 @@
 package rockpaperscissors;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner in = new Scanner(System.in);
+        switch (in.next()) {
+            case "rock" -> System.out.println("Sorry, but the computer chose paper");
+            case "paper" -> System.out.println("Sorry, but the computer chose scissors");
+            case "scissors" -> System.out.println("Sorry, but the computer chose rock");
+        }
     }
 }
