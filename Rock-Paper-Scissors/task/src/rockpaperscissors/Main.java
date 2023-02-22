2ea2d67e5728a612378c32e6d5c87873b4c92658
SunnyPanda <ekaterina.kozlova@canter.fi>
2023-02-22 21:37:56 +0200

    Stage 4/5: Scoring the game

--- Rock-Paper-Scissors/task/src/rockpaperscissors/Main.java
+++ Rock-Paper-Scissors/task/src/rockpaperscissors/Main.java
@@ -1,5 +1,7 @@
 package rockpaperscissors;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
@@ -13,18 +15,41 @@ public class Main {
         Scanner in = new Scanner(System.in);
         Random random = new Random();
 
+        System.out.print("Enter your name: ");
+        String name = in.next();
+        System.out.printf("Hello, %s\n", name);
+
+        int rating = 0;
+        File file = new File("./rating.txt");
+        try (Scanner inFile = new Scanner(file)) {
+            while (inFile.hasNext()) {
+                if (inFile.next().equals(name)) rating = inFile.nextInt();
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
         String user = in.next();
         while (!user.equals("!exit")) {
-            if (!options.contains(user)) System.out.println("Invalid input");
+            if (user.equals("!rating")) System.out.printf("Your rating: %d\n", rating);
+            else if (!options.contains(user)) System.out.println("Invalid input");
             else {
                 int index = random.nextInt(3);
                 String comp = options.get(index);
 
-                if (user.equals(comp)) System.out.printf("There is a draw (%s)\n", user);
-                else if (comp.equals(bits.get(user))) System.out.printf("Well done. The computer chose %s and failed\n", comp);
+                if (user.equals(comp)) {
+                    System.out.printf("There is a draw (%s)\n", user);
+                    rating += 50;
+                }
+                else if (comp.equals(bits.get(user))) {
+                    System.out.printf("Well done. The computer chose %s and failed\n", comp);
+                    rating += 100;
+                }
                 else System.out.printf("Sorry, but the computer chose %s\n", comp);
             }
             user = in.next();
         }
+
+
     }
 }
